56d4fb22796e5f4126e85a9f287045fc6bf64a8c
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-15 01:39:09 -0400

    Begun implementing some of the file methods

--- Fileb.go
+++ Fileb.go
@@ -3,31 +3,60 @@ package fileb
 import (
 	"errors"
 	"os"
+	"path/filepath"
 )
 
 type Fileb struct {
 	Path string
 }
 
-func NewFileb(path string) *Fileb {
-	return &Fileb{
-		Path: path,
+func NewFileb(path string) (*Fileb, error) {
+	absFilePath, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
 	}
+	return &Fileb{
+		Path: absFilePath,
+	}, nil
+}
+
+func (filebSelf *Fileb) GetFileInfo() (os.FileInfo, error) {
+	fileInfo, err := os.Stat(filebSelf.Path)
+	if os.IsNotExist(err) {
+		return nil, errors.New("File does not exist.")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fileInfo, nil
+
 }
 
 func (filebSelf *Fileb) IsExecutable() bool {
-	panic("Method not yet implemented")
+	fileInfo, err := filebSelf.GetFileInfo()
+	if err != nil {
+		return false
+	}
+	mode := fileInfo.Mode().Perm()
+	//TODO UNTESTED.  Test this.
+	return mode|0111 == 0
 }
 
 func (filebSelf *Fileb) IsExecutableByCurrentUser() bool {
+	//TODO this will need to be a mix of these:
+	// https://groups.google.com/forum/#!topic/golang-nuts/ywS7xQYJkHY
+	// http://golang.org/pkg/os/#Geteuid
 	panic("Method not yet implemented")
 }
 
 func (filebSelf *Fileb) IsDirectory() bool {
-	panic("Method not yet implemented")
-}
-
-func (filebSelf *Fileb) GetFileInfo() os.FileInfo {
+	fileInfo, err := filebSelf.GetFileInfo()
+	if err != nil {
+		return false
+	}
+	return fileInfo.IsDir()
 	panic("Method not yet implemented")
 }
 
